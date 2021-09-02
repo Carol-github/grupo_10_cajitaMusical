7112ef85a29acf3aed2337569bdca3c7b6fe00c5
Mauro Ruiz <mauror@MacBook-Pro-de-Mauro.local>
2021-09-02 17:50:41 -0300

    subida de app.js modificado

--- app.js
+++ app.js
@@ -11,3 +11,15 @@ app.listen(3030,()=>{
 app.get ("/",(req, res) => {
     res.sendFile (path.resolve("./views/index.html"));
 });
+app.get ("/detalle_producto",(req, res) => {
+    res.sendFile (path.resolve("./views/product_detail.html"));
+});
+app.get ("/carrito",(req, res) => {
+    res.sendFile (path.resolve("./views/cart.html"));
+});
+app.get ("/registro",(req, res) => {
+    res.sendFile (path.resolve("./views/register.html"));
+});
+app.get ("/login",(req, res) => {
+    res.sendFile (path.resolve("./views/login.html"));
+});
