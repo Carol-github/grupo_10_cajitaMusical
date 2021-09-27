f383de0f287407d3a15478bea5363a8ba99f8df7
Mauro Ruiz <mauror@MacBook-Pro-de-Mauro.local>
2021-09-27 16:14:49 -0300

    agregado de productos al listado

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,5 +1,10 @@
 const path = require('path');
+const fs = require('fs');
 
+/* En la constante "products" ya tienen los productos que están 
+guardados en la carpeta Data como Json (un array de objetos literales) */
+const productsFilePath = path.join(__dirname, '../data/dataProducts.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 const productsController = {
     productCart: (req, res) => {
         //res.sendFile(path.join(__dirname, '../views/products/productCart.html'));
@@ -11,7 +16,10 @@ const productsController = {
     },
     productList: (req, res) => {
         //res.sendFile(path.join(__dirname, '../views/products/productDetail.html'));
-        res.render('products/productList');
+        // res.render('products/productList');
+        res.render('products/productList.ejs',{
+			products: products
+		});
     }
 }
 
