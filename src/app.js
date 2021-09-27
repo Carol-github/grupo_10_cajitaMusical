be0efa6c4f641d3fdbc621e77842df245f15beb0
CHLUQUE <luque.christian@gmail.com>
2021-09-27 02:06:06 -0300

    Cambios en el main controller para empezar a usar la repeticion de productos en Index

--- src/app.js
+++ src/app.js
@@ -24,18 +24,3 @@ app.use('/productos', productsRoutes);
 
 
 
-/* app.get ("/",(req, res) => {
-    res.sendFile (path.resolve("./views/index.html"));
-});
-app.get ("/detalle_producto",(req, res) => {
-    res.sendFile (path.resolve("./views/productDetail.html"));
-});
-app.get ("/carrito",(req, res) => {
-    res.sendFile (path.resolve("./views/productCart.html"));
-});
-app.get ("/registro",(req, res) => {
-    res.sendFile (path.resolve("./views/register.html"));
-});
-app.get ("/login",(req, res) => {
-    res.sendFile (path.resolve("./views/login.html"));
-}); */
--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,10 +1,31 @@
+const fs = require('fs');
 const path = require('path');
 
-const mainController = {
-    index: (req, res) => {
+/* En la constante "products" ya tienen los productos que están 
+guardados en la carpeta Data como Json (un array de objetos literales) */
+const productsFilePath = path.join(__dirname, '../data/dataProducts.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+//const mainController = {
+  //  index: (req, res) => {
         //res.render(path.join(__dirname, '../views/index/index.ejs'));
-        res.render('index/index');
-    }
-}
+    //    res.render('index/index');
+   // }
+//}
+
+
+const mainController = {
+	index: (req, res) => {
+		const products_in_sale = products.filter(product => {
+			return product.oferta == "true"
+		});
+        console.log(products_in_sale);
+		res.render('index.ejs',{
+			products_in_sale: products_in_sale
+		})
+	}
+};
+
+
 
 module.exports = mainController;
