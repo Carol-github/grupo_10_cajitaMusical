f74014969da20590e79d06c59a73553fca0b89b1
maurordf <mauror.df@gmail.com>
2021-10-26 17:05:24 -0300

    session

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -3,6 +3,9 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 
+// Middlewares
+const authMiddleware = require('../middlewares/authMiddleware');
+const guestMiddleware = require('../middlewares/guestMiddleware');
 //aca definimos donde se van a guardar las imagenes de los usuarios
 const diskStorage = multer.diskStorage({ 
     destination: function (req, file, cb) { 
@@ -22,16 +25,16 @@ const upload = multer({ storage: diskStorage });
 
 const usersController = require('../controllers/usersController.js');
 
-router.get('/ingresar', usersController.login);
+router.get('/ingresar', guestMiddleware, usersController.login);
 router.post('/ingresar', usersController.process_login);
 
-router.get('/registrar', usersController.register);
+router.get('/registrar', guestMiddleware, usersController.register);
 router.post('/registrar', upload.single('avatar'), usersController.store)
 
-router.get('/admin', usersController.admin);
+router.get('/admin', authMiddleware, usersController.admin);
 
 
-router.get('/modificar', usersController.modify);
+router.get('/modificar', authMiddleware, usersController.modify);
 
 
 module.exports = router;
