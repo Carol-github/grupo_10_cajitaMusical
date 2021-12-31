919e5eab55fe54c6969cdea541edf42b77573c3e
plugdelsur <luque.christian@gmail.com>
2021-12-31 03:22:53 -0300

    APIs completo

--- src/controllers/api/apiProductsController.js
+++ src/controllers/api/apiProductsController.js
@@ -1,87 +1,72 @@
 //const req = require("../../database/models");
 const db = require("../../database/models");
 const sequelize = db.sequelize;
-const Op =db.sequelize.Op;
-
+const Op = db.sequelize.Op;
 
 const apiProductsController = {
-
-  index: (req,res)=>{
-
-
+  index: (req, res) => {
     let countCategory = db.Products.count({
-        include: ['categoria'],
-        where: {deleted:0},
-        col: 'fk_category',
-        group: ['categoria.category_name']
-    })
+      include: ["categoria"],
+      where: { deleted: 0 },
+      col: "fk_category",
+      group: ["categoria.category_name"],
+    });
 
-    let allProducts = db.Products
-    .findAll(
-         {
-        order:[['id', 'DESC']],
-        attributes:['id', 'title', 'description'],
-        where: {deleted : 0},
-        include:['categoria']
-    }
-    );
+    let allProducts = db.Products.findAll({
+      order: [["id", "DESC"]],
+      attributes: ["id", "title", "description"],
+      where: { deleted: 0 },
+      include: ["categoria"],
+    });
 
-    Promise.all(
-        [allProducts, countCategory]
-    )
-    .then(([products, countCategory]) =>{
-        propCategory= [];
-        valueCategory= [];
-        for(i=0;i<countCategory.length;i++){
-            propCategory.push(countCategory[i].category_name)
-            valueCategory.push(countCategory[i].count)
-        }
+    Promise.all([allProducts, countCategory])
+      .then(([products, countCategory]) => {
+        products = products
+          .map((el) => el.get({ plain: true }))
+          .map((product) => {
+            product.url = `http://localhost:3031/api/productos/${product.id}`;
+            product.dbRelations = ["fk_category"];
+            return product;
+          });
 
-        let countByCategory ={};
+        let countByCategory = {};
+        countCategory.forEach((obj) => {
+          countByCategory[obj.category_name] = obj.count;
+        });
 
-        propCategory.forEach((categoria,index)=>countByCategory[categoria] = valueCategory[index])
-
-
-
-        let result ={
-            metadata:{
-                url: req.originalUrl,
-                quantity:products.length,
-                categoryByQuantity:countByCategory
-
-              //countByCategory:[...countByCategory]
-            },
-            data: products,
-            // categories: categoria
-        }
+        let result = {
+          metadata: {
+            url: req.originalUrl,
+            quantity: products.length,
+            categoryByQuantity: countByCategory,
+          },
+          data: products,
+        };
         return res.send(result);
-    })
-    .catch(error=>console.log(error));
-},
-        productDetail: (req, res) => {
-            let findById = 
-            db.Products
-            .findOne({
-              where: { id: req.params.id, deleted: 0 },
-            });
-    
-            Promise.all([findById])
-            .then((product) => {              
-              if (product[0] === null) {
-                res.send("El producto no existe");
-              } else {
-                product[0].image = `http://localhost:3031/img/imgProducts/${product[0].image}`;
-                let productToSend = {
-                  meta: {
-                    status: 200,
-                    total: product.length,
-                    url: req.originalUrl                
-                  },
-                  data: product,
-                };
-                res.json(productToSend);
-              }
-            }); 
-        }
-}
+      })
+      .catch((error) => console.log(error));
+  },
+  productDetail: (req, res) => {
+    let findById = db.Products.findOne({
+      where: { id: req.params.id, deleted: 0 },
+    });
+
+    Promise.all([findById]).then((product) => {
+      if (product[0] === null) {
+        res.send("El producto no existe");
+      } else {
+        product[0].image = `http://localhost:3031/img/imgProducts/${product[0].image}`;
+        let productToSend = {
+          meta: {
+            status: 200,
+            total: product.length,
+            url: req.originalUrl,
+          },
+          data: product,
+        };
+        res.json(productToSend);
+      }
+    });
+  },
+};
 module.exports = apiProductsController;
--- src/controllers/api/apiUsersController.js
+++ src/controllers/api/apiUsersController.js
@@ -2,54 +2,58 @@
 // const { getEventListeners } = require("mysql2/typings/mysql/lib/Connection");
 const db = require("../../database/models");
 const sequelize = db.sequelize;
-const Op =db.sequelize.Op;
-
+const Op = db.sequelize.Op;
 
 const apiUserController = {
-    index: (req,res)=>{
-        let allUsers = 
-        db.Users.findAll({
-          order: [["id", "DESC"]],
-          attributes: ["id", "user", "email"],
-          where: { deleted: 0 },
-        })
-          .then((users) => {
-            let result = {
-              metadata: {
-                url: req.originalUrl,
-                quantity: users.length,
-              },
-              data: users,
-            };
-            return res.send(result);
-          })
-          .catch((error) => console.log(error));
-    },
-    userDetail: (req, res) => {
-        let findById =
-        db.Users.findOne(
-            { where: { id: req.params.id, deleted: 0 }, 
-            attributes: {exclude: ['password', 'category_id', 'deleted']} 
-            });
+  index: (req, res) => {
+    let allUsers = db.Users.findAll({
+      order: [["id", "DESC"]],
+      attributes: ["id", "user", "email"],
+      where: { deleted: 0 },
+    })
+      .then((users) => {
+        users = users
+          .map((el) => el.get({ plain: true }))
+          .map((user) => {
+            user.url = `http://localhost:3031/api/usuarios/${user.id}`;
+            return user;
+          });
+        console.log(users);
+        let result = {
+          metadata: {
+            url: req.originalUrl,
+            quantity: users.length,
+          },
+          data: users,
+        };
+        // console.log(result)
+        return res.send(result);
+      })
+      .catch((error) => console.log(error));
+  },
+  userDetail: (req, res) => {
+    let findById = db.Users.findOne({
+      where: { id: req.params.id, deleted: 0 },
+      attributes: { exclude: ["password", "category_id", "deleted"] },
+    });
 
-        Promise.all([findById])
-        .then((user) => {
-          if (user[0] === null) {
-            res.send("El usuario no existe");
-          } else {
-            user[0].image = `http://localhost:3031/img/avatars/${user[0].image}`;
-            let userToSend = {
-              meta: {
-                status: 200,
-                total: user.length,
-                url: req.originalUrl                
-              },
-              data: user,
-            };
-            res.json(userToSend);
-          }
-        }); 
-    } 
-}
-       
-module.exports = apiUserController
+    Promise.all([findById]).then((user) => {
+      if (user[0] === null) {
+        res.send("El usuario no existe");
+      } else {
+        user[0].image = `http://localhost:3031/img/avatars/${user[0].image}`;
+        let userToSend = {
+          meta: {
+            status: 200,
+            total: user.length,
+            url: req.originalUrl,
+          },
+          data: user,
+        };
+        res.json(userToSend);
+      }
+    });
+  },
+};
+
+module.exports = apiUserController;
