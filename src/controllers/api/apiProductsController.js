dcb076f8957f8a12ea79d11dc1478648a90b809d
maurordf <mauror.df@gmail.com>
2021-12-31 02:06:44 -0300

    prod index fixed

--- src/controllers/api/apiProductsController.js
+++ src/controllers/api/apiProductsController.js
@@ -6,54 +6,58 @@ const Op =db.sequelize.Op;
 
 const apiProductsController = {
 
-        index: (req,res)=>{
-            // let countByCategory = 
-            // sequelize.query("SELECT t2.category_name , COUNT(*) FROM products AS t1, product_categories AS t2 WHERE t1.fk_category = t2.id GROUP BY fk_category")
+  index: (req,res)=>{
 
-            let countByCategory = db.Products
-            .findAll({
-                attributes: { 
-                    include: [[sequelize.fn("COUNT", sequelize.col("categoria.id")), "categoria.category_name"]] 
-                },
-                include:['categoria'],
-                group: ['fk_category']
-            })
 
-            let allProducts = db.Products
-            .findAll(
-                 {
-                  order:[['id', 'DESC']],
-                  attributes:['id', 'title', 'description'],
-                  where: {deleted : 0},
-                  include:['categoria']
-               
-            }
-            );
-            Promise.all(
-                [allProducts, countByCategory]
-            )
+    let countCategory = db.Products.count({
+        include: ['categoria'],
+        where: {deleted:0},
+        col: 'fk_category',
+        group: ['categoria.category_name']
+    })
 
-            
+    let allProducts = db.Products
+    .findAll(
+         {
+        order:[['id', 'DESC']],
+        attributes:['id', 'title', 'description'],
+        where: {deleted : 0},
+        include:['categoria']
+    }
+    );
 
-            .then(([products, countByCategory]) =>{
-                // if (products.url == null){
-                //     for(i=1 ; products.length ; i++){
-                //         products[i].url = `http://localhost:3031/productos/${products.id}`;; 
-                //     }                          
-                // }  
-                       
-                let result ={
-                    metadata:{
-                        url: req.originalUrl,
-                        quantity:products.length,
-                      countByCategory:[...countByCategory]
-                    },
-                    data: products,
-                }
-                return res.send(result);
-            })
-            .catch(error=>console.log(error));
-        },
+    Promise.all(
+        [allProducts, countCategory]
+    )
+    .then(([products, countCategory]) =>{
+        propCategory= [];
+        valueCategory= [];
+        for(i=0;i<countCategory.length;i++){
+            propCategory.push(countCategory[i].category_name)
+            valueCategory.push(countCategory[i].count)
+        }
+
+        let countByCategory ={};
+
+        propCategory.forEach((categoria,index)=>countByCategory[categoria] = valueCategory[index])
+
+
+
+        let result ={
+            metadata:{
+                url: req.originalUrl,
+                quantity:products.length,
+                categoryByQuantity:countByCategory
+
+              //countByCategory:[...countByCategory]
+            },
+            data: products,
+            // categories: categoria
+        }
+        return res.send(result);
+    })
+    .catch(error=>console.log(error));
+},
         productDetail: (req, res) => {
             let findById = 
             db.Products
